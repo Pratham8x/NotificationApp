57e722acfffa010f44245df21b7da120d4460617
PrathamPandey <pratham.pandey@astraltron.com>
2026-08-30 01:41:03 +0530

    railway url added

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,2 @@
-import {Platform} from 'react-native';
-
-const host = Platform.OS === 'android' ? '10.0.2.2' : 'localhost';
-export const SERVER_URL = `http://${host}:5000`;
+export const SERVER_URL = 'https://chirpy-production.up.railway.app';
 export const API_URL = `${SERVER_URL}/api`;
